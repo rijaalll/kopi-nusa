1392c4b72fa4fa6744ff8a33c95f00b5e060bc87
Ahmad Rizal <rizalajipermana@gmail.com>
2025-04-24 07:01:47 +0700

    update firebase module

--- src/module/firebase.js
+++ src/module/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get, update } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6c29IF2vgFZunbhZ4Uk8khaB2jKqrpBA",
-    authDomain: "kopi-nusa-api.firebaseapp.com",
-    databaseURL: "https://kopi-nusa-api-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "kopi-nusa-api",
-    storageBucket: "kopi-nusa-api.firebasestorage.app",
-    messagingSenderId: "940546892696",
-    appId: "1:940546892696:web:1c08daf37d39b7f77ec1ed",
-    measurementId: "G-XFGSPB7WYY"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
